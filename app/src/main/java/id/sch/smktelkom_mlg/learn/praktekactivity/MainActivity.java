b7b627b68bc272b0144c16a305d6e51afe1b6719
cikaandhini <begidakancika120200@gmail.com>
2016-10-13 12:57:59 +0700

    Livecycle Music

--- app/src/main/java/id/sch/smktelkom_mlg/learn/praktekactivity/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/praktekactivity/MainActivity.java
@@ -1,54 +1,68 @@
 package id.sch.smktelkom_mlg.learn.praktekactivity;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "LivecycleTag";
+    private static final String  Tag = "LivecycleTag";
+    MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "onCreate: ");
+        Log.d(Tag, "onCreate: ");
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart: ");
+        Log.d(Tag, "onStart: ");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume:");
+        Log.d(Tag, "onResume:");
+        mediaPlayer.start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause: ");
+        Log.d(Tag, "onPause: ");
+        mediaPlayer.pause();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop: ");
+        Log.d(Tag, "onStop: ");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "onRestart: ");
+        Log.d(Tag, "onRestart: ");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy: ");
+        Log.d(Tag, "onDestroy: ");
+
+        if (mediaPlayer != null)
+            mediaPlayer.release();
+
     }
+
+
+
 }
